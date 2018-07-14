b3988e04c75e2df852b78f040e9f167a1209b6ad
Shaoying Chan <739791373@qq.com>
2018-07-14 21:01:07 +0800

    first commit

---
+++ src/main/java/cn/darren/ferrari/auth/FerrariAuthApplication.java
@@ -0,0 +1,12 @@
+package cn.darren.ferrari.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerrariAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FerrariAuthApplication.class, args);
+    }
+}
---
+++ src/test/java/cn/darren/ferrari/auth/FerrariAuthApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.darren.ferrari.auth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FerrariAuthApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
